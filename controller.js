b7bd48920e6cc0d88c5cb5a894947bc2343994c9
KamyC <caojinghan123@hotmail.com>
2019-03-05 21:31:51 -0800

    revise a bit

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#courses").css("display","none")
     $(".left-nav").click(function () {
         $(".left-nav").css("font-weight","normal");
-        $(this).css({"font-weight":"bold","font-size":"17px"})
+        $(this).css({"font-weight":"bold","font-size":"16px"})
     })
     $("#expansion").click(function () {
         $("#courses").fadeToggle()
