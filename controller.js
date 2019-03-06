4fb66d2f10a6a17b5403e8269709c1b85f9e90ea
KamyC <caojinghan123@hotmail.com>
2019-03-05 21:30:01 -0800

    done

--- controller.js
+++ controller.js
@@ -1,3 +1,11 @@
-window.onload=function () {
-    alert('works here')
-}
+$(document).ready(function(){
+    $("#courses").css("display","none")
+    $(".left-nav").click(function () {
+        $(".left-nav").css("font-weight","normal");
+        $(this).css({"font-weight":"bold","font-size":"17px"})
+    })
+    $("#expansion").click(function () {
+        $("#courses").fadeToggle()
+    })
+
+});
